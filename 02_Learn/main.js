e94fc48468f23483a2558651136b8467873d1a99
raopremrao <raom61337@gmail.com>
2025-08-06 15:32:15 +0530

    Completed Episode 2

--- 02_Learn/main.js
+++ 02_Learn/main.js
@@ -1,9 +1,10 @@
-gsap.from("#page1 #box", {
-    scale: 0,
-    delay: 1,
-    duration: 1,
-    rotate: 360,
-})
+// gsap.from("#page1 #box", {
+//     scale: 0,
+//     delay: 1,
+//     duration: 1,
+//     rotate: 360,
+// })
+
 
 // gsap.from("#page2 #box", {
 //     scale: 0,
@@ -16,6 +17,38 @@ gsap.from("#page1 #box", {
 //         start: "top 60%"
 //     },
 // });
+
+
+gsap.from("#page3 #box", {
+    opacity: 0,
+    scale: 0,
+    // duration: 1.5,
+    x: 500,
+    rotate: 720,
+    // borderRadius: "50%",
+    scrollTrigger: {
+        trigger: "#page3 #box",
+        markers: true,
+        scroller: "body",
+        start: "top 90%",
+        scrub: 2,
+        pin: true
+    },
+    borderRadius: "50%"
+})
+
+gsap.from("#page2 h2", {
+    opacity: 0,
+    scale: 0,
+    duration: 2,
+    x: -500,
+    scrollTrigger: {
+        trigger: "#page2 h2",
+        // markers: true,
+        scroller: "body",
+        start: "top 70%"
+    }
+})
 
 
 
